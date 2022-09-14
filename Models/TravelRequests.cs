00b729fe727b7721c879d97f16a33d985b79a7f4
Ayorinde G. Smart <smartayorinde@gmail.com>
2022-09-14 14:13:04 +0100

    Removed Null constructs

--- Models/TravelRequests.cs
+++ Models/TravelRequests.cs
@@ -5,16 +5,16 @@ namespace TRSRestAPI.Models
     {
         [Key]
         public int RequestID { get; set; }
-        public DateTime? RequestDate { get; set; }
-        public string? SourceLocation { get; set; }
-        public string? SourceCountry { get; set; }
-        public string? DestinationLocation { get; set; }
-        public string? DestinationCountry { get; set; }
+        public DateTime RequestDate { get; set; }
+        public string SourceLocation { get; set; }
+        public string SourceCountry { get; set; }
+        public string DestinationLocation { get; set; }
+        public string DestinationCountry { get; set; }
         public DateTime DepartureTimestamp { get; set; }
-        public string? TravelClass { get; set; }
-        public string? TripType { get; set; }
-        public string? ChargeCode { get; set; }
-        public string? TravelerName { get; set; }
-        public string? RequestStatus { get; set; }
+        public string TravelClass { get; set; }
+        public string TripType { get; set; }
+        public string ChargeCode { get; set; }
+        public string TravelerName { get; set; }
+        public string RequestStatus { get; set; }
     }
 }
